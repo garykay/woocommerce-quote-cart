f78f5bef33c6160c418b0f3fc5630523fd3bc103
Joshua <donesells@yahoo.com>
2013-07-31 16:35:08 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ woocommerce-quote-cart
 ======================
 
 woocommerce quote cart 
+
+this is a modified version of woocommerce 2.0.13
+
+It is ment to be uploaded to a woothemes woocommerce folder. 
+The modified folder structure should be as follows
+content/theme/woocommerce/......
+admin
+cart
+checkout
+css
+emails
+order
+single-product
+
+the plugin file 
+woocommerce-functions.php must also be uploaded to
+
+plugins/woocommerce/woocommerce-functions.php
---
+++ woocommerceQuoteCart/templates/checkout/review-order.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Review order form
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+global $woocommerce;
+
+$available_methods = $woocommerce->shipping->get_available_shipping_methods();
+?>
+<div id="order_review">
+
+	<table class="shop_table">
+		<thead>
+			<tr>
+				<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+				
+			</tr>
+		</thead>
+		
+		<tbody>
+			<?php
+				do_action( 'woocommerce_review_order_before_cart_contents' );
+
+				if (sizeof($woocommerce->cart->get_cart())>0) :
+					foreach ($woocommerce->cart->get_cart() as $cart_item_key => $values) :
+						$_product = $values['data'];
+						if ($_product->exists() && $values['quantity']>0) :
+							echo '
+								<tr class="' . esc_attr( apply_filters('woocommerce_checkout_table_item_class', 'checkout_table_item', $values, $cart_item_key ) ) . '">
+									<td class="product-name">' .
+										apply_filters( 'woocommerce_checkout_product_title', $_product->get_title(), $_product ) . ' ' .
+										apply_filters( 'woocommerce_checkout_item_quantity', '<strong class="product-quantity">&times; ' . $values['quantity'] . '</strong>', $values, $cart_item_key ) .
+										$woocommerce->cart->get_item_data( $values ) .
+									'</td>
+									
+								</tr>';
+						endif;
+					endforeach;
+				endif;
+
+				do_action( 'woocommerce_review_order_after_cart_contents' );
+			?>
+		</tbody>
+	</table>
+
+	<div id="payment">
+		<?php if ($woocommerce->cart->needs_payment()) : ?>
+		<ul class="payment_methods methods">
+			<?php
+				$available_gateways = $woocommerce->payment_gateways->get_available_payment_gateways();
+				if ( ! empty( $available_gateways ) ) {
+
+					// Chosen Method
+					if ( isset( $woocommerce->session->chosen_payment_method ) && isset( $available_gateways[ $woocommerce->session->chosen_payment_method ] ) ) {
+						$available_gateways[ $woocommerce->session->chosen_payment_method ]->set_current();
+					} elseif ( isset( $available_gateways[ get_option( 'woocommerce_default_gateway' ) ] ) ) {
+						$available_gateways[ get_option( 'woocommerce_default_gateway' ) ]->set_current();
+					} else {
+						current( $available_gateways )->set_current();
+					}
+
+					foreach ( $available_gateways as $gateway ) {
+						?>
+						<li>
+							<input type="radio" id="payment_method_<?php echo $gateway->id; ?>" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> />
+							<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
+							<?php
+								if ( $gateway->has_fields() || $gateway->get_description() ) :
+									echo '<div class="payment_box payment_method_' . $gateway->id . '" ' . ( $gateway->chosen ? '' : 'style="display:none;"' ) . '>';
+									$gateway->payment_fields();
+									echo '</div>';
+								endif;
+							?>
+						</li>
+						<?php
+					}
+				} else {
+
+					if ( ! $woocommerce->customer->get_country() )
+						echo '<p>' . __( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) . '</p>';
+					else
+						echo '<p>' . __( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) . '</p>';
+
+				}
+			?>
+		</ul>
+		<?php endif; ?>
+
+		<div class="form-row place-order">
+
+			<noscript><?php _e( 'Since your browser does not support JavaScript, or it is disabled, please ensure you click the <em>Update Totals</em> button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce' ); ?><br/><input type="submit" class="button alt" name="woocommerce_checkout_update_totals" value="<?php _e( 'Update totals', 'woocommerce' ); ?>" /></noscript>
+
+			<?php $woocommerce->nonce_field('process_checkout')?>
+
+			<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
+
+			<?php
+			$order_button_text = apply_filters('woocommerce_order_button_text', __( 'Place quote', 'woocommerce' ));
+
+			echo apply_filters('woocommerce_order_button_html', '<input type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . $order_button_text . '" />' );
+			?>
+
+			<?php if (woocommerce_get_page_id('terms')>0) : ?>
+			<p class="form-row terms">
+				<label for="terms" class="checkbox"><?php _e( 'I have read and accept the', 'woocommerce' ); ?> <a href="<?php echo esc_url( get_permalink(woocommerce_get_page_id('terms')) ); ?>" target="_blank"><?php _e( 'terms &amp; conditions', 'woocommerce' ); ?></a></label>
+				<input type="checkbox" class="input-checkbox" name="terms" <?php checked( isset( $_POST['terms'] ), true ); ?> id="terms" />
+			</p>
+			<?php endif; ?>
+
+			<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
+
+		</div>
+
+		<div class="clear"></div>
+
+	</div>
+
+</div>
